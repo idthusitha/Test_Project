aa0c3c18d0b44b6bd8a9aa8b514c2db60dde18a9
thusitha <thusitha.dissanayaka@rezgateway.com>
2019-01-07 10:36:05 +0530

    add

--- src/FileRead.java
+++ src/FileRead.java
@@ -1,27 +1,40 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class FileRead {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		BufferedReader br = new BufferedReader(new FileReader("ride_jetty_log"));
-		try {
-		    StringBuilder sb = new StringBuilder();
-		    String line = br.readLine();
+	public static void main(String[] args) {
 
-		    while (line != null) {
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
-		        line = br.readLine();
-		    }
-		    String everything = sb.toString();
-		}catch(Exception e){
-			e.printStackTrace();			
-		} finally {
-		    br.close();
+		try {
+			File f = new File(FileRead.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+			File dir = f.getAbsoluteFile().getParentFile();
+			String path = dir.toString();
+
+			BufferedReader br = new BufferedReader(new FileReader(path + "/src/ride_jetty_log.txt"));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+					if (line.indexOf("qry-->") != -1) {
+						sb.append(line);
+						sb.append(System.lineSeparator());
+						line = br.readLine();
+					}
+				}
+				String everything = sb.toString();
+
+				System.out.println(everything);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				br.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 	}
