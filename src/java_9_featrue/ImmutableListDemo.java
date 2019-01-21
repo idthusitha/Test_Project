47a52afbcb890ea872e4a148bc754ec7500ec285
thusitha <thusitha.dissanayaka@rezgateway.com>
2019-01-21 12:42:15 +0530

    add java 11 feature changes

---
+++ src/java_9_featrue/ImmutableListDemo.java
@@ -0,0 +1,21 @@
+package java_9_featrue;
+
+import java.util.List;
+
+import org.apache.poi.util.SystemOutLogger;
+
+public class ImmutableListDemo {
+
+	public static void main(String[] args) {
+		String version = System.getProperty("java.version");
+
+		System.out.println(version);
+
+		List immutableList = List.of();
+		System.out.println(immutableList);
+		
+		List immutableList2 = List.of("one","two","three");
+		System.out.println(immutableList2);
+	}
+
+}
