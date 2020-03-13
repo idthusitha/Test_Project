b71402358d718288f21c4d08f06fc7deef120b89
thusitha <thusitha.dissanayaka@rezgateway.com>
2020-03-13 12:02:58 +0530

    added

--- src/logstash/LogstashDemo.java
+++ src/logstash/LogstashDemo.java
@@ -22,8 +22,8 @@ public class LogstashDemo {
 		directTCPCall();
 		System.out.println(System.currentTimeMillis() - start);
 
-		start = System.currentTimeMillis();
-		asyTCPCall();
+		//start = System.currentTimeMillis();
+		//asyTCPCall();
 		System.out.println(System.currentTimeMillis() - start);
 
 	}
@@ -32,8 +32,9 @@ public class LogstashDemo {
 		try {
 			System.out.println("aaaa");
 
-			// Socket socket = new Socket("172.16.16.212", 5400);
-			Socket socket = new Socket("logstash.mon.prod.ride.use1.aws.rezos.io", 6044);
+			Socket socket = new Socket("172.16.16.212", 5400);
+			//Socket socket = new Socket("logstash.mon.prod.ride.use1.aws.rezos.io", 6044);
+			//Socket socket = new Socket("10.32.12.82", 6044);
 
 			DataOutputStream os = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 
@@ -53,12 +54,19 @@ public class LogstashDemo {
 
 			/*******************/
 
-			String fileName = "/var/log/rezg/app/activity/2020-03-02/tixrez/ESB_Response_3.json";
+			String fileName = "/var/log/rezg/app/activity/2020-03-02/tixrez/ESB_Response_9.json";
 
 			String logTextPattern = "";
-			logTextPattern += "@@ENV_START@@" + "cib";
-			logTextPattern += "@@URL_START@@" + fileName.replace("/var/", "");
-			logTextPattern += "@@CONTENT_START@@" + json.toString() + "@@CONTENT_END@@";
+			logTextPattern += "@@FIELDS@@" +"{\"source\": \"ride_sup_logs\",\"env\": \"lmt\",\"type\": \"supp\",\"name\": \"wso2ei-common\",\"portal\": \"ride\"}";
+			logTextPattern += "@@URL@@" + fileName;
+			logTextPattern += "@@CONTENT_START@@" + json.toString().replace("\n", "") + "@@CONTENT_END@@";
+			
+			System.out.println(logTextPattern);
+			//@@FIELDS@@{"source": "ride_sup_logs","env": "lmt","type": "supp","name": "wso2ei-common","portal": "ride"}@@URL@@/var/log/rezg/app/activity/2020-03-02/tixrez/ESB_Response_9.json@@CONTENT_START@@{"request":{"supplierId":"hbactivity","portal":"tixrez","checkIn":"2020-03-19T00:00:00","checkOut":"2020-03-20T00:00:00","currencyCode":"USD","RequestID":"101010","traceId":"11594","markup":{"value":[0.12,0.12,0.12,0.12]},"language":"en","noOfNights":3,"adultsNum":2,"childNum":0,"items":null,"country":"55","city":"3086"}}@@CONTENT_END@@
+			//@@FIELDS@@%{GREEDYDATA:esb_fields}@@URL@@%{GREEDYDATA:esb_url}@@CCONTENT_START@@%{GREEDYDATA:esb_file_content}@@CONTENT_END@@
+			
+			//@@ENV@@%{GREEDYDATA:esb_env}@@SERVICE@@{GREEDYDATA:service}@@URL@@%{GREEDYDATA:esb_file_name}@@CONTENT_START@@%{GREEDYDATA:esb_file_content}@@CONTENT_END@@
+
 
 			os.writeBytes(logTextPattern);
 			os.flush();
@@ -79,13 +87,13 @@ public class LogstashDemo {
 			Future<Void> future = client.connect(hostAddress);
 			future.get();
 			
-			String fileName = "/var/log/rezg/app/activity/2020-03-02/tixrez/ESB_Response_3.json";
+			String fileName = "/var/log/rezg/app/activity/2020-03-12/tixrez/ESB_Response_3.json";
 			net.sf.json.JSONObject json = net.sf.json.JSONObject.fromObject(readFile());
 			
 			String logTextPattern = "";
-			logTextPattern += "@@ENV_START@@" + "cib";
-			logTextPattern += "@@URL_START@@" + fileName.replace("/var/", "");
-			logTextPattern += "@@CONTENT_START@@" + json.toString() + "@@CONTENT_END@@";
+			logTextPattern += "@@FIELDS@@" +"{\"source\": \"ride_sup_logs\",\"env\": \"lmt\",\"type\": \"supp\",\"name\": \"wso2ei-common\",\"portal\": \"ride\"}";
+			logTextPattern += "@@URL@@" + fileName;
+			logTextPattern += "@@CONTENT_START@@" + json.toString().replace("\n", "") + "@@CONTENT_END@@";
 
 			
 			byte[] byteMsg = new String(logTextPattern).getBytes();
@@ -101,7 +109,7 @@ public class LogstashDemo {
 
 	private static String readFile() throws FileNotFoundException, IOException {
 		String everything = "";
-		try (BufferedReader br = new BufferedReader(new FileReader("/rezsystem/workspace_ride/Test_Project/src/logstash/test.json"))) {
+		try (BufferedReader br = new BufferedReader(new FileReader("/rezsystem/workspace_ride/Test_Project/src/logstash/test3.json"))) {
 			StringBuilder sb = new StringBuilder();
 			String line = br.readLine();
 
